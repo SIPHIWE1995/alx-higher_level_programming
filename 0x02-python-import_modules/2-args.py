04675d162e1a1d19a68112dee07568ae445ceec1
SIPHIWE1995 <siphiwencane21@gmail.com>
2023-08-16 07:56:19 -0700

    python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
+    from sys import argv
+    i = 0
+    length = len(argv)
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    if length == 2:
+        print("{} argument:".format(length - 1))
+    elif length == 1:
+        print("{} arguments.".format(length - 1))
     else:
-        print("{} arguments:".format(i))
+        print("{} arguments:".format(length - 1))
 
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
+    for arg in argv:
+        if i == 0:
             i += 1
+            continue
+        print("{}: {}".format(i, arg))
+        i += 1
--- 0x02-python-import_modules/variable_load_5.py
+++ 0x02-python-import_modules/variable_load_5.py
@@ -1,7 +1,2 @@
-#!/usr/bin/python3
-a = 98
-"""Simple variable
-"""
-
-guillaume@ubuntu:~/0x02$ ./5-variable_load.py
-98
+#!/usr/bin/bash
+a = 35
