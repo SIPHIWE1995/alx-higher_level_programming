2a01478018de149e5397c409fe75815064578f08
SIPHIWE1995 <siphiwencane21@gmail.com>
2023-08-16 07:28:12 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-first_num = 97
-second_num 123
-letter_find = 'q'
-second_find = 'e'
-for alphabet in range(first_num, second_num):
-    if chr(alphabet) is not letter_find and chr(alphabet) is not second_find:
-        print("{}".format(chr(alphabet)), end=""))
+for i in "abcdefghijklmnopqrstuvwxyz":
+    if i == "q" or i == "e":
+        continue
+    print("{}".format(i), end="")
